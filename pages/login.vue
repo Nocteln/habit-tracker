8add7b8fd5dae1646e62f56a1ad28ba542260edf
Nocteln <eliottmieze@gmail.com>
2024-08-06 12:35:03 +0200

    debut de la stylisation de la page login

--- pages/login.vue
+++ pages/login.vue
@@ -15,16 +15,24 @@ definePageMeta({
 </script>
 
 <template>
-  <div>sign in</div>
+  <div class="text-center h-[50vh]">
+    <h1 class="text-3xl font-bold py-5 text-slate-50">sign in / register</h1>
 
-  <button @click="handleGoogle" class="bg-green-400">
-    Register with google
-  </button>
-  <button @click="handleDiscord" class="bg-blue-400">
-    Register with discord
-  </button>
-  <div v-if="data">
-    {{ data }}
+    <div class="flex item-center justify-center m-5 pt-12">
+      <button
+        @click="handleGoogle"
+        class="bg-green-400 hover:bg-green-500 hover:font-bold p-8 md:p-12 flex justify-center items-center flex-col"
+      >
+        with google
+        <Img src="logo_google.png" class="h-12 hidden md:block pt-5" />
+      </button>
+      <button
+        @click="handleDiscord"
+        class="bg-blue-400 hover:bg-blue-500 hover:font-bold p-8 md:p-12 flex justify-center items-center flex-col"
+      >
+        with discord
+        <Img src="logo_discord.png" class="h-12 hidden md:block" />
+      </button>
+    </div>
   </div>
-  <div v-else>pas de data</div>
 </template>
