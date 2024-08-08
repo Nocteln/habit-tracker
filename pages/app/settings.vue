1ca9b3d861fef87953466cf236bcb8230239e445
Nocteln <eliottmieze@gmail.com>
2024-08-08 20:34:04 +0200

    debut du setting

--- pages/app/settings.vue
+++ pages/app/settings.vue
@@ -1,7 +1,64 @@
 <template>
-  <div class="h-[70vh] flex justify-center items-center flex-col">
-    <h1 class="text-3xl pb-12">Comming soon</h1>
+  <div class="flex flex-col items-center">
+    <h1 class="text-2xl font-bold m-7">personal informations</h1>
+    <UForm
+      :schema="schema"
+      :state="state"
+      class="space-y-4 md:space-y-0 flex flex-col md:flex-row items-center"
+      @submit="onSubmit"
+    >
+      <!-- <UFormGroup label="Email" name="email">
+        <UInput v-model="state.email" />
+      </UFormGroup> -->
+      <div class="md:mr-4 space-y-4">
+        <UFormGroup label="Name" name="password">
+          <UInput v-model="state.name" type="text" />
+        </UFormGroup>
+        <UFormGroup label="Username" name="password">
+          <UInput v-model="state.username" type="text" />
+        </UFormGroup>
+      </div>
 
-    <UButton to="/app">Go home</UButton>
+      <UButton type="submit" size="xl" color="amber"> Edit </UButton>
+    </UForm>
+    <div class="flex flex-col items-center">
+      <h1 class="text-2xl font-bold m-7 text-red-500">sensitive area</h1>
+      <UButton color="red">Reset my data</UButton>
+      <UButton color="red">Delete my account</UButton>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import { useUserStore } from "~/store/user";
+import { z } from "zod";
+import type { FormSubmitEvent } from "#ui/types";
+
+const user = useUserStore();
+await user.fetch();
+
+const schema = z.object({
+  email: z.string().email("Invalid email"),
+  name: z
+    .string()
+    .min(3, "Must be at least 3 characters")
+    .max(25, "Can't be more than 25 characters"),
+  username: z
+    .string()
+    .min(3, "Must be at least 3 characters")
+    .max(25, "Can't be more than 25 characters"),
+});
+
+type Schema = z.output<typeof schema>;
+
+const state = reactive({
+  email: user.email,
+  name: user.name,
+  username: user.username,
+});
+
+async function onSubmit(event: FormSubmitEvent<Schema>) {
+  // Do something with data
+  console.log(event.data);
+}
+</script>
